c0141487c706ae2b3d3068dfc1a8265b6d2794a7
anna_tama <anna.tama.roma@gmail.com>
2022-09-03 17:05:37 -0300

    init: Ciclo de vida de componentes

--- my-store/src/app/components/img/img.component.ts
+++ my-store/src/app/components/img/img.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit,OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges,AfterViewInit,OnDestroy {
 
   @Input() img: string = ''; //el padre lo comunica al hijo
   @Output() loaded = new EventEmitter<string>(); //comunica del hijo al padre
   imageDefault  = '../../../assets/images/default.png';
 
-  constructor() { }
+  constructor() {
+    //before render
+    //no va nada asíncrono como fecth, peticiones al server, suscripción
+    console.log('constructor', 'imgValue =>', this.img)
+  }
+
+  //before render
+  //cambios en inputs
+  ngOnChanges(){
+    console.log('ngOnChanges', 'imgValue =>', this.img)
+  }
 
   ngOnInit(): void {
+    //before render
+    //async fetch, llamadas a las apis -
+    console.log('ngOnInit', 'imgValue =>', this.img)
   }
+
+  ngAfterViewInit(){
+    //after render
+    //handler children
+    console.log('ngAfterViewInit', 'imgValue =>', this.img)
+  }
+
+  ngOnDestroy(){
+    //delete
+    console.log('ngOnDestroy', 'imgValue =>', this.img)
+  }
+
 
   imgError(){
     this.img= this.imageDefault;
