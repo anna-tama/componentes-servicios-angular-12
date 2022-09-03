559990917d9fcdf51a9835d5fa960b3e795843f2
anna_tama <anna.tama.roma@gmail.com>
2022-09-03 16:00:20 -0300

    init: Componente para producto

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product } from './models/product.model'
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,26 @@ export class AppComponent {
 
   // imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
   imgParent= '';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Automobil de juguete',
+      price: 100,
+      image: './assets/images/toy.jpg'
+  },
+  {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 180,
+      image: './assets/images/bike.jpg'
+  },
+  {
+      id: '3',
+      name: 'Colección de albumes',
+      price: 120,
+      image: './assets/images/album.jpg'
+  }
+  ]
 
   onLoaded(img: string){
     console.log('log padre',img);
