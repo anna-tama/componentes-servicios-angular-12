2a4cdd7b49ead9c9fea017d9ea8ea3e5b84870dd
anna_tama <anna.tama.roma@gmail.com>
2022-09-10 11:37:11 -0300

    init: Componentes y Header

--- my-store/src/app/components/products/products.component.ts
+++ my-store/src/app/components/products/products.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {Product} from  '../../models/product.model'
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Product
+} from '../../models/product.model'
 
 @Component({
   selector: 'app-products',
@@ -8,30 +13,40 @@ import {Product} from  '../../models/product.model'
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  products: Product[] = [
-    {
+  products: Product[] = [{
       id: '1',
-      name: 'Automobil de juguete',
+      name: 'Juguete',
       price: 100,
       image: './assets/images/toy.jpg'
-  },
-  {
+    },
+    {
       id: '2',
       name: 'Bicicleta casi nueva',
       price: 180,
       image: './assets/images/bike.jpg'
-  },
-  {
+    },
+    {
       id: '3',
       name: 'Colección de albumes',
       price: 120,
       image: './assets/images/album.jpg'
-  }
+    },
+    {
+      id: '4',
+      name: 'Casa',
+      price: 120,
+      image: './assets/images/house.jpg'
+    },
+    {
+      id: '5',
+      name: 'Libros',
+      price: 120,
+      image: './assets/images/books.jpg'
+    }
   ]
 
 }
